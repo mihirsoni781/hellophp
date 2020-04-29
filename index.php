81cad8046680e7d0219ab1c12a263f500d5189bc
Mihir Soni <51269450+mihirsoni781@users.noreply.github.com>
2020-04-29 16:29:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+ ?>
+<style type="text/css">
+	body{
+		margin:0px;
+		font-family: segoe UI;
+	}
+</style>
+ <body>
+ 	
+ 	<div class="header" style="height: 100px; width: 100%; background-color: black;color:white;">
+ 		<span style="font-size: 30px; font-weight: 300;  position:relative; top:25px; left: 20px;">MIHIR SONI <span style="color: gray;">PHOTOGRAPHY</span></span>
+ 		<img src="mihirsoni.png" style="height: 70px; float:right; position:relative; top:15px; right: 15px;">
+ 	</div>
+ </body>
